174a2437fb1f1f33233df58b6af50e3b2c5865d7
Arthur Pajola <108372587+PajolaArthur@users.noreply.github.com>
2023-05-29 11:06:49 -0300

    Update model.py

--- model.py
+++ model.py
@@ -3,13 +3,13 @@ from datetime import *
 
 class Usuarios(db.Model):
     id = db.Column(db.Integer, primary_key = True) # Chave primária auto
-    created = db.Column(db.DateTime, default=datetime.utcnow) # Gerado no momento de insert
+    created = db.Column(db.DateTime, default=datetime.now) # Gerado no momento de insert
     nome = db.Column(db.String(40), nullable = False) # admin
     senha = db.Column(db.String(40), nullable = False) # admin
 
 class Coletas(db.Model):
     id = db.Column(db.Integer, primary_key = True) # Chave primária auto
-    created = db.Column(db.DateTime, default=datetime.utcnow) # Gerado no momento de insert
+    created = db.Column(db.DateTime, default=datetime.now) # Gerado no momento de insert
     descricao = db.Column(db.String(60), nullable = False) # Fogão
     quantidade = db.Column(db.String(3), nullable = False) # 2
     endereco = db.Column(db.String(40), nullable = False) # Praça Coronel Francisco Orlando 600
